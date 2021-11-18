f71a14e90068c03cfbf22c7e12ebf2cfd5f8ea27
bhejno99 <54297833+beatahejno@users.noreply.github.com>
2021-11-17 23:48:35 -0500

    added sp to the main activity

--- app/src/main/java/com/example/cs403project2/MainActivity.java
+++ app/src/main/java/com/example/cs403project2/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.cs403project2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 
@@ -15,6 +16,11 @@ public class MainActivity extends AppCompatActivity {
 
         //stories manager object is to organize all of the stories, categories, etc. into one class
         StoriesManager storiesManager = new StoriesManager();
+
+        //use shared preferences to get the category of stories that the user needs right now
+        SharedPreferences pref = getSharedPreferences("StoriesSP", MODE_PRIVATE);
+        //for example, it is bright outside
+        String category = pref.getString("bright", "dark");
 
     }
 
