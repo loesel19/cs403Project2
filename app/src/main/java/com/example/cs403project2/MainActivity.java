7b21bd4ec5d5b82d3b3ac88012291141218504a0
Alexander Buckstiegel <awbuckst@svsu.edu>
2021-11-19 13:37:02 -0500

    GPS implemented to MainActivity

--- app/src/main/java/com/example/cs403project2/MainActivity.java
+++ app/src/main/java/com/example/cs403project2/MainActivity.java
@@ -1,25 +1,43 @@
 package com.example.cs403project2;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import static com.google.android.gms.common.GooglePlayServicesUtil.isGooglePlayServicesAvailable;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+
 import com.android.volley.Request;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_LOCATION_PERMISSION = 1;
     String category; //category of the story that's going to be displayed in StoryActivity
 
     final static String TAG = "weather";
     private double lat;
     private double lon;
     private String weather;
+    private FusedLocationProviderClient fusedLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +53,36 @@ public class MainActivity extends AppCompatActivity {
         category = pref.getString("bright", "dark");
 
 
-        RequestSingleton.getInstance(this).addToRequestQueue(requestObj(43.62,-83.92));
+        //GPS Stuff
+
+        //Check for permissions
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                //If no, request the permission fro the user
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_LOCATION_PERMISSION);
+        }
+        else {
+            //otherwise just note that permission was already granted
+            Log.d("gps", "getLocation: permissions granted");}
+
+        //Gets last location from phone - exact isn't really necessary
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            //Set lat and lon values to the location lat and long
+                            lat = location.getLatitude();
+                            lon = location.getLongitude();
+                            Log.d("gps", "Latitude: " + lat + " Longitude: " + lon);
+                            //Add to api queue to get the weather from these coordinates
+                            RequestSingleton.getInstance(getApplicationContext()).addToRequestQueue(requestObj(lat, lon));
+                        }
+                    }
+                });
     }
+
 
     public void launchSettings(View view) {
         Intent intent = new Intent(this, Settings.class);
